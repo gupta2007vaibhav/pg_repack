e78f0b53fb8f92ad8f82fa76da9343a01105503d
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2017-03-26 00:07:38 +0000

    Change directory before sudoing as postgres

--- regress/travis_prepare.sh
+++ regress/travis_prepare.sh
@@ -9,4 +9,7 @@ export PATH="$PGBIN:$PATH"
 
 sudo apt-get install postgresql-server-dev-$PGVER
 
+# Go somewhere else or sudo will fail
+cd /
 sudo -u postgres "$PGBIN/pg_ctl" -w -l /dev/null -D "$DATADIR" start
+cd -
